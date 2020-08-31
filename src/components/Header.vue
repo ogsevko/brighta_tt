27763dea83963092b7eeca49c88f834569394dc1
Yaroslav Andriyenko <andriyenko.yaroslav@gmail.com>
2020-08-31 16:08:28 +0300

    Added main component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -59,109 +59,109 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  .header {
-    font-family: "Montserrat", Arial, Helvetica, sans-serif;
+.header {
+  font-family: "Montserrat", Arial, Helvetica, sans-serif;
 
-    margin-bottom: 48px;
+  margin-bottom: 48px;
 
-    &__container {
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      align-items: center;
-    }
-
-    &__logo {
-      display: flex;
-      flex-direction: row;
-      align-items: flex-end;
-      height: 26px;
-    }
-
-    &__image {
-      display: block;
-      width: 83.61px;
-      height: 14.78px;
-      object-fit: contain;
-      margin-right: 9px;
-    }
-
-    &__subtitle {
-      font-weight: normal;
-      font-size: 13px;
-      line-height: 140%;
-      align-self: flex-start;
-    }
-
-    &__settings {
-      min-width: 322px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-    }
-
-    &__notification-button {
-      border: none;
-      outline: none;
-
-      width: 43px;
-      height: 43px;
-      background: rgb(255, 255, 255);
-      box-shadow: 0 10px 40px rgba(213, 221, 230, 0.6);
-      border-radius: 50%;
-    }
-
-    &__user {
-      width: 120px;
-      height: 43px;
-      background: rgb(255, 255, 255);
-      box-shadow: 0 10px 40px rgba(213, 221, 230, 0.6);
-      border-radius: 100px;
-
-      font-weight: bold;
-      font-size: 14px;
-      line-height: 17px;
-      text-align-last: center;
-
-      border: none;
-      outline: none;
-    }
-
-    &__user-option {
-      font-weight: bold;
-      font-size: 14px;
-      line-height: 17px;
-      text-align: center;
-    }
-
-    &__search {
-      border: none;
-      outline: none;
-      border-radius: 50%;
-
-      width: 43px;
-      height: 43px;
-      background: rgba(203, 215, 228, 0.4);
-
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-
-    &__language {
-      width: 64px;
-      height: 43px;
-      background: rgba(203, 215, 228, 0.4);
-      border-radius: 100px;
-
-      font-style: normal;
-      font-weight: bold;
-      font-size: 14px;
-      line-height: 17px;
-      text-align-last: center;
-
-      border: none;
-      outline: none;
-    }
+  &__container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
   }
+
+  &__logo {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    height: 26px;
+  }
+
+  &__image {
+    display: block;
+    width: 83.61px;
+    height: 14.78px;
+    object-fit: contain;
+    margin-right: 9px;
+  }
+
+  &__subtitle {
+    font-weight: normal;
+    font-size: 13px;
+    line-height: 140%;
+    align-self: flex-start;
+  }
+
+  &__settings {
+    min-width: 322px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__notification-button {
+    border: none;
+    outline: none;
+
+    width: 43px;
+    height: 43px;
+    background: rgb(255, 255, 255);
+    box-shadow: 0 10px 40px rgba(213, 221, 230, 0.6);
+    border-radius: 50%;
+  }
+
+  &__user {
+    width: 120px;
+    height: 43px;
+    background: rgb(255, 255, 255);
+    box-shadow: 0 10px 40px rgba(213, 221, 230, 0.6);
+    border-radius: 100px;
+
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 17px;
+    text-align-last: center;
+
+    border: none;
+    outline: none;
+  }
+
+  &__user-option {
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 17px;
+    text-align: center;
+  }
+
+  &__search {
+    border: none;
+    outline: none;
+    border-radius: 50%;
+
+    width: 43px;
+    height: 43px;
+    background: rgba(203, 215, 228, 0.4);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &__language {
+    width: 64px;
+    height: 43px;
+    background: rgba(203, 215, 228, 0.4);
+    border-radius: 100px;
+
+    font-style: normal;
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 17px;
+    text-align-last: center;
+
+    border: none;
+    outline: none;
+  }
+}
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="main">
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Main",
+}
+</script>
+
+<style scoped lang="scss">
+.main {
+  position: relative;
+}
+</style>
